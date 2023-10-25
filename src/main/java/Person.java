3a4bc1f4a70b3b6c790a6595ea0bafadd38bab1f
Lean Obregon <lean.obregon@fiserv.com>
2023-10-25 14:49:28 -0400

    Ex 1: Person

---
+++ src/main/java/Person.java
@@ -0,0 +1,50 @@
+public class Person {
+    String fName, lName;
+    byte age;
+    int num;
+    static int count = 0;
+    public Person() {
+        fName = "";
+        lName = "";
+        count++;
+    }
+
+    public Person(String fName, String lName) {
+        this.fName = fName;
+        this.lName = lName;
+        count++;
+    }
+
+    public Person(String fName, String lName, byte age, int num) {
+        this.fName = fName;
+        this.lName = lName;
+        this.age = age;
+        this.num = num;
+        count++;
+    }
+
+    public String speak() {
+        if (!fName.isEmpty() && age > 0) {
+            return "My name is " + this.fName + " " + this.lName + " and I am " + this.age + " years old.";
+        }
+        if (!fName.isEmpty()) {
+            return "My name is " + this.fName + " " + this.lName;
+        }
+        return "I don't have a name.";
+    }
+
+    public static int getPersonCount() {
+        return count++;
+    }
+
+    @Override
+    public String toString() {
+        if (!fName.isEmpty() && num > 0) {
+            return this.fName + " " + this.lName + " " + this.num%10000;
+        }
+        if (!fName.isEmpty()) {
+            return this.fName + " " + this.lName;
+        }
+        return "N/A";
+    }
+}
